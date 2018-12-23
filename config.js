4b7ae161d8893b36fa8a6d9f58ccf8af47733548
nab <nab@get-hey.com>
2018-12-23 12:48:06 +0100

    config

---
+++ config.js
@@ -0,0 +1,11 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+
+exports._ = {
+    api_path: 'https://ide.hey.network/post',
+    txt: {
+        input_box_placeholder: 'Ask your question here',
+        input_box_prompt: 'Ask your question here, try to be as specific as possible. You can skip this step and ask your question on the Hey site afterwards if, for instance, you want to add emojis 😆',
+        hey_callback_message: 'Your question is here: '
+    }
+};
--- extension.js
+++ extension.js
@@ -1,20 +1,16 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
+var CONFIG = require('./config')._;
 var editor = vscode.window.activeTextEditor;
 var rp = require("request-promise");
 
 function PostQuestion(_question, _code) {
-    // vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('https://ide.hey.network/post?q='+_question+'c='+_code));
+    vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(CONFIG.api_path+'?q='+_question+'c='+_code));
 }
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "hey" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
@@ -32,11 +28,11 @@ function activate(context) {
         // User Input to ask the question
         vscode.window.showInputBox({
             ignoreFocusOut: true,
-            placeHolder: "Ask your question here",
-            prompt: "Ask your question here, try to be as specific as possible. You can skip this step and ask your question on the Hey site afterwards if, for instance, you want to add emojis 😆"
+            placeHolder: CONFIG.txt.input_box_placeholder,
+            prompt: CONFIG.txt.input_box_prompt
         }).then(function(_userInput){
             // Display a message box to the user
-            vscode.window.showInformationMessage('Hey! Your code has '+text.length+' characters and the question was: '+ _userInput);
+            vscode.window.showInformationMessage(CONFIG.txt.hey_callback_message + _userInput);
             PostQuestion(_userInput, text);
         });
 
