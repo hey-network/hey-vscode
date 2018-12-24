46860b4f33a8da58ce8abcd4882eac8194186ba5
nab <nab@get-hey.com>
2018-12-24 18:54:19 +0100

    get ridded of excessive files

--- extension.js
+++ extension.js
@@ -1,9 +1,18 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
-var CONFIG = require('./config')._;
 var editor = vscode.window.activeTextEditor;
 var rp = require("request-promise");
+
+var CONFIG = {
+    app_path: 'https://ide.hey.network/c',
+    api_path: 'https://api2.get-hey.com/v1/ide',
+    txt: {
+        input_box_placeholder: 'Ask your question here',
+        input_box_prompt: 'Ask your question here, try to be as specific as possible. You can skip this step and ask your question on the Hey site afterwards if, for instance, you want to add emojis 😆',
+        hey_callback_message: 'Your question is here: '
+    }
+};
 
 function PostQuestion(_question, _code, _language) {
 
